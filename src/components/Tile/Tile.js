28981f78180943b37ee3ad04c45c219965b04f4b
crogmi <chrisogami16@gmail.com>
2021-07-27 16:38:29 -0700

    Cleaned up naming convention

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -11,20 +11,13 @@ const Tile = (props) => {
     const name = props.obj.name;
     const itemNumber = props.obj.itemNumber;
 
-    const images = {
-        'Coffee': CoffeeImg,
-        'Espresso': EspressoImg,
-        'Latte': LatteImg,
-        'Cold Brew': ColdbrewImg
-    };
-
-    const array = [LatteImg, CoffeeImg, ColdbrewImg, EspressoImg];
+    const images = [LatteImg, CoffeeImg, ColdbrewImg, EspressoImg];
     
     return (
-        <div className='tile-container'>
+        <div className='tile'>
             <Link to={`/shop/${itemNumber}`}>
-                <img src={array[itemNumber]} 
-                     alt={images.name}
+                <img src={images[itemNumber]} 
+                     alt={name}
                      key={itemNumber + 1} />
             </Link>
             <h3 className='tile' key={itemNumber + 1}>
